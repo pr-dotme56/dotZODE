59e4ff4e83aee7fdc62ca2c6dfe6c54db5c631f8
Abhinav PR <135339339+iamAbhinavPR@users.noreply.github.com>
2023-09-02 19:38:22 +0530

    Update Encode.svelte

--- src/lib/Encode.svelte
+++ src/lib/Encode.svelte
@@ -25,12 +25,10 @@
     <div class="inputField">
         <input bind:value={str} placeholder="Enter the text" type="text" class="inputText bg-gray-200 border border-gray-300 text-gray-900 text-sm w-full rounded-md focus:ring-blue-500 focus:border-blue-500 block p-3 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"/>
         {#if str}
-            <span>
-            <button class="clearBtn" on:click={ClearIn}>
+            <span class="clearBtn" on:click={ClearIn}>
                     <svg class="w-6 h-6 text-gray-800 dark:text-white inline-flex" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
                         <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 11.793a1 1 0 1 1-1.414 1.414L10 11.414l-2.293 2.293a1 1 0 0 1-1.414-1.414L8.586 10 6.293 7.707a1 1 0 0 1 1.414-1.414L10 8.586l2.293-2.293a1 1 0 0 1 1.414 1.414L11.414 10l2.293 2.293Z"/>
                     </svg>
-            </button>
             </span>
         {/if}
     </div>
