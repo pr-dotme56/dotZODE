bd36fce1c11c96674a1ff8566c48fde8a1e5dde1
Abhinav PR <135339339+iamAbhinavPR@users.noreply.github.com>
2023-09-03 21:09:38 +0530

    Update Encode.svelte

--- src/lib/Encode.svelte
+++ src/lib/Encode.svelte
@@ -23,14 +23,12 @@
 <div class="window">
 
     <div class="inputField">
-        <input bind:value={str} placeholder="Enter the text" type="text" class="inputText bg-gray-200 border border-gray-300 text-gray-900 text-sm w-full rounded-md focus:ring-blue-500 focus:border-blue-500 block p-3 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"/>
+        <input bind:value={str} placeholder="Enter the text" type="text" class="inputText pr-10 mb-2.5 h-12 relative bg-gray-200 border border-gray-300 text-gray-900 text-sm w-full rounded-md focus:ring-blue-500 focus:border-blue-500 block p-3 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"/>
         {#if str}
             <span class="clearBtn" on:click={ClearIn}>
-                <div class="clearBtnBackground">
-                    <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
                         <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 11.793a1 1 0 1 1-1.414 1.414L10 11.414l-2.293 2.293a1 1 0 0 1-1.414-1.414L8.586 10 6.293 7.707a1 1 0 0 1 1.414-1.414L10 8.586l2.293-2.293a1 1 0 0 1 1.414 1.414L11.414 10l2.293 2.293Z"/>
-                    </svg>
-                </div>
+                </svg>
             </span>
         {/if}
     </div>
@@ -60,30 +58,19 @@
 
 
 <style>
-    .clearBtnBackground {
-        background-color: #334155;
-        padding: 5px; 
-    }
     .clearBtn {
         position: absolute;
         float: right;
         right: 10px;
         padding: 10px;
         top: 0%;
-        cursor: pointer;
-        
+        cursor: pointer; 
     }
     .inputField{
         position: relative;
         display: flex;
         align-items: center;
         justify-content: center;
-    }
-    .inputText{
-        margin-bottom: 10px;
-        position: relative;
-        height: 48;
-        padding-right: 40px;
     }
     .code{
         padding: 25px;
