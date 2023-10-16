62fb61692d620bb70d4f66b73c450a6f0e9dd52a
Abhinav PR <135339339+iamAbhinavPR@users.noreply.github.com>
2023-10-16 21:04:47 +0530

    Update Conversion.svelte

--- src/lib/Conversion.svelte
+++ src/lib/Conversion.svelte
@@ -211,7 +211,7 @@
     </div>
     
     <div class="relative w-96 rounded-lg mt-4">
-        <p class="textarea overflow-x-auto whitespace-pre">{code}</p>
+        <pre class="textarea overflow-x-auto whitespace-pre"><code>{code}</code></pre>
         <button on:click={copyClick} use:clipboard={code} class="absolute top-2 right-2 p-2 btn-sm btn variant-soft-surface">
             {!copyState ? btnLabel : btnCopied }
         </button>
